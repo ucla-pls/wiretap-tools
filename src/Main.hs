eecb8aac1abd0284942b10a6d0046918ac44495b
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-09-19 17:16:20 -0700

    Add Command line interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,39 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import System.IO
+import           System.IO
+import           System.Environment (getArgs)
+import           System.Console.Docopt
 
-import Control.Monad
+import           Control.Monad
 
-import Wiretap.Data.Event
-import Wiretap.Format.Binary
+import           Wiretap.Data.Event
+import           Wiretap.Format.Binary
+
+
+patterns :: Docopt
+patterns = [docopt|
+wiretap-tools version 0.1.0.0
+
+Usage:
+   wiretap-tools parse [--type TYPE] <file>
+   wiretap-tools (-h | --help | --version)
+|]
 
 main :: IO ()
 main = do
-  ((Event t o i opr) : events) <- readEvents (Thread 0) stdin
+  args <- parseArgsOrExit patterns =<< getArgs
 
-  forM_ events print
+  when (args `isPresent` longOption "help"
+        || args `isPresent` shortOption 'h') $ do
+    exitWithUsage patterns
+
+  when (args `isPresent` command "parse") $ do
+    file <- args `getArgOrExit` argument "file"
+    withFile file ReadMode $ \h -> do
+      events <- readEvents (Thread 0) h
+      putStrLn $ "Successfully parsed " ++ show (length events) ++ " events";
+
+
+  where
+    getArgOrExit = getArgOrExitWith patterns
