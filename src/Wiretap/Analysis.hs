a0ed14654a6da82390906fbcfb4259f6abe7216c
Christian Gram Kalhauge <christian@kalhauge.dk>
2016-09-29 21:15:00 -0700

    More code

--- src/Wiretap/Analysis.hs
+++ src/Wiretap/Analysis.hs
@@ -4,6 +4,7 @@
 module Wiretap.Analysis where
 
 import           Control.Monad.State
+import           Control.Monad
 import           Control.Lens hiding ((...))
 
 import           Data.Monoid
@@ -18,41 +19,11 @@ import qualified Z3.Base
 import           Wiretap.Data.Event(Event(..), Operation(..), Thread(..))
 import qualified Wiretap.Data.Event as Event
 
-data Order = Order { from :: Int, to :: Int } deriving (Show)
-
-infixl 8 ~>
-(~>) = Order
-
-
-update :: Monoid b => Lens' a b -> a -> a -> a
-update l a = l <>~ (a ^. l)
-
-data MhbData =
-  MhbData { _mhbFork  :: Last Int
-          , _mhbBegin :: Last Int
-          , _mhbEnd   :: Last Int
-          , _mhbJoins :: [Int]
-          } deriving (Show)
-makeLenses ''MhbData
-
-emptyMhbData = MhbData mempty mempty mempty mempty
-
-instance Monoid MhbData where
-  mempty = emptyMhbData
-  mappend a b = update mhbFork b
-              . update mhbBegin b
-              . update mhbEnd b
-              . update mhbJoins b
-              $ a
 
 (...) = (.) . (.)
 
-newtype Mhb = Mhb { _mhbMap :: IM.IntMap MhbData } deriving (Show)
-makeLenses ''Mhb
-
-instance Monoid Mhb where
-  mempty = Mhb IM.empty
-  mappend = Mhb ... IM.unionWith (<>) `on` _mhbMap
+infixl 8 ~>
+(~>) = Order
 
 with :: Monoid a => Lens' a b -> b -> a
 with l v = mempty & l .~ v
@@ -60,45 +31,135 @@ with l v = mempty & l .~ v
 withM :: (Monoid a, Monad m) => Lens' a (m b) -> b -> a
 withM l v = mempty & l .~ return v
 
-atThread :: Thread -> Lens' Mhb (Maybe MhbData)
-atThread t = mhbMap . at (threadId t)
+newtype IdMap a = IdMap { innerMap :: IM.IntMap a} deriving (Show)
 
-withThread :: Monad m => Thread -> Lens' MhbData (m a) -> a -> Mhb
-withThread t l v = withM (atThread t) $ withM l v
+instance Monoid a => Monoid (IdMap a) where
+  mempty = IdMap IM.empty
+  mappend = IdMap ... IM.unionWith (<>) `on` innerMap
 
--- Calculate the must happen before, expects the events to be
--- in order in the threads.
-mhb :: Int -> Event -> Mhb
-mhb i (Event {thread=t, operation=o}) =
-  case o of
-    Begin   -> withThread t mhbBegin i
-    End     -> withThread t mhbEnd i
-    Fork t' -> withThread t' mhbFork i
-    Join t' -> withThread t' mhbJoins i
-    _ -> mempty
+instance Foldable IdMap where
+  foldMap f = foldMap f . innerMap
 
-_Last :: Prism' (Last a) a
-_Last = prism' (Last . Just) getLast
+atId :: Monoid a => Int -> a -> IdMap a
+atId = IdMap ... IM.singleton
 
-mhbOrders :: Mhb -> [Order]
-mhbOrders = IM.foldMapWithKey (const mhbDataOrders) . (^. mhbMap)
-  where
-    mhbDataOrders a =
-      case (a ^? mhbFork . _Last, a ^? mhbBegin . _Last) of
-        (Just i, Just j) -> [ i ~> j ]
-        _ -> []
-      ++
-      case a ^? mhbEnd . _Last of
-        Just e -> map (e ~>) $ a ^. mhbJoins
-        _ -> []
+atThread :: Monoid a => Thread -> a -> IdMap a
+atThread = atId . threadId
+
+onAll :: Applicative f => f (a -> b) -> a -> f b
+onAll a = (a <*>) . pure
+
+type EventId = Int
+
+data Order = Order
+  { from :: EventId
+  , to :: EventId
+  } deriving (Show)
+
+class Monoid a => OrderAnalysis a where
+  fromEvent :: EventId -> Event -> a
+  toOrders :: a -> [Order]
+
+fromEvents :: OrderAnalysis a => [Event] -> a
+fromEvents =
+  mconcat . L.zipWith fromEvent [0..]
+
+-- | orders, takes two lists and create orders between every event in the first
+-- list and to every event in the second list.
+orders :: [EventId] -> [EventId] -> [Order]
+orders = liftM2 Order
+
+-- | total, takes a list and create orders so that they the containing events
+-- are total ordered.
+total :: [EventId] -> [Order]
+total = ap (zipWith Order) tail
+
+-- Must Happen Before Analysis
+data MhbData = MhbData
+  { _mhbForks  :: [EventId]
+  , _mhbBegins :: [EventId]
+  , _mhbEnds   :: [EventId]
+  , _mhbJoins  :: [EventId]
+  } deriving (Show)
+makeLenses ''MhbData
+
+instance Monoid MhbData where
+  mempty = MhbData mempty mempty mempty mempty
+  mappend a b =
+    MhbData
+      { _mhbForks = merge _mhbForks
+      , _mhbBegins = merge _mhbBegins
+      , _mhbEnds = merge _mhbEnds
+      , _mhbJoins = merge _mhbJoins
+      }
+   where merge f = on mappend f a b
+
+newtype MHB = MHB { mhb :: IdMap MhbData }
+
+instance Monoid MHB where
+  mempty = MHB mempty
+  mappend = MHB ... mappend `on` mhb
+
+instance OrderAnalysis MHB where
+  fromEvent i (Event {thread=t, operation=o}) =
+    MHB $ case o of
+      Begin   -> atThread t  $ withM mhbBegins i
+      End     -> atThread t  $ withM mhbEnds i
+      Fork t' -> atThread t' $ withM mhbForks i
+      Join t' -> atThread t' $ withM mhbJoins i
+      _ -> mempty
+
+  toOrders =
+    foldMap mhbDataOrders . mhb
+    where
+      mhbDataOrders :: MhbData -> [Order]
+      mhbDataOrders =
+        concat . onAll
+          [ order mhbForks mhbBegins
+          , order mhbEnds mhbJoins
+          ]
+      order f1 f2 a = on orders (a ^.) f1 f2
 
 
+-- Sequential Consistency
+newtype SC = SC { cs :: IdMap [EventId] }
+
+instance Monoid SC where
+  mempty = SC mempty
+  mappend = SC ... mappend `on` cs
+
+-- Sequential Consistency expects all envents to be represented in order
+instance OrderAnalysis SC where
+  fromEvent i (Event {thread = t}) =
+    SC $ atThread t [i]
+
+  toOrders = foldMap total . cs
+
+-- Lock Consistency
+
+data LcLockTriple = LcLockTriple
+  { _lltAcquire :: Maybe EventId
+  , _lltRequire :: Maybe EventId
+  , _lltRelease :: Maybe EventId
+  }
+
+data LcData = LcData [LcLockTriple]
+
+type LC = IdMap [EventId]
+
+lock :: EventId -> Event -> LC
+lock i e = undefined
+
+-- Top level analyses
 
 linearizeTotal :: [Event] -> [Event]
 linearizeTotal = id
 
 linearizeTotal' :: [Event] -> IO ()
 linearizeTotal' events = do
-  let mhb' = mconcat $ L.zipWith mhb [0..] events
-  print mhb'
-  print (mhbOrders mhb')
+  let mhb' = fromEvents events :: MHB
+  print (toOrders mhb')
+  let sc' = fromEvents events :: SC
+  print (toOrders sc')
+  -- let lc' = fromEvents events :: LC
+  -- print (toOrders lc')
